c96638c62952b2005e4ac76b3b8a2b4e68689fe2
Max Hendricks <go4811@wayne.edu>
2020-02-11 23:33:28 -0500

    Testing Phase

--- NCOrganizer/NCOrganizer/notepadScreen.xaml.cs
+++ NCOrganizer/NCOrganizer/notepadScreen.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace NCOrganizer
 {
@@ -20,14 +21,56 @@ namespace NCOrganizer
     /// </summary>
     public partial class notepadScreen : Window
     {
-        static string path = @"C:\Users\Max Hendricks\Documents\NotecardNotes\";
+        static string path = @"";
+        string[] tempFiles;
         string name = "";
-        string[] files = Directory.GetFiles(path);
+        
         List<Button> listOfButtons = new List<Button>();
 
         public notepadScreen()
         {
             InitializeComponent();
+        }
+
+        
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            // Shows the next window
+            MainWindow newWindow = new MainWindow();
+            newWindow.Show();
+            this.Close();
+        }
+
+        private void Add_Card(object sender, RoutedEventArgs e)
+        {
+            Forms newForm = new Forms();
+            newForm.Show();
+            this.Close();
+        }
+
+        private void Direct_Path(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button).Content.ToString(); // Figures out button clicked.
+
+            for (int i = 0; i < listOfButtons.Count; i++)
+            {
+                if (listOfButtons.ElementAt(i).Content.ToString() == content)
+                {
+                    Process.Start(tempFiles[i]); // starts files at location where i was found.
+                }
+            }
+        }
+
+        private void Set_Path(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog getFilePath = new OpenFileDialog();
+            
+
+            getFilePath.ShowDialog();
+
+            path += getFilePath.FileName.ToString();
+
+            string[] files = Directory.GetFiles(path);
 
             for (int i = 0; i < files.Length; i++)
             {
@@ -40,7 +83,7 @@ namespace NCOrganizer
                 name = files[i];
                 newButton.Click += Direct_Path;
                 newButton.Content = Path.GetFileName(files[i]);
-               
+
                 listOfButtons.ElementAt(i).Click += Direct_Path;
                 listOfButtons.ElementAt(i).Content = Path.GetFileName(files[i]);
             }
@@ -69,36 +112,19 @@ namespace NCOrganizer
                 }
 
             }
-            
-        }
 
-        
-        private void Home_Click(object sender, RoutedEventArgs e)
-        {
-            // Shows the next window
-            MainWindow newWindow = new MainWindow();
-            newWindow.Show();
-            this.Close();
-        }
-
-        private void Add_Card(object sender, RoutedEventArgs e)
-        {
-            Forms newForm = new Forms();
-            newForm.Show();
-            this.Close();
-        }
-
-        private void Direct_Path(object sender, RoutedEventArgs e)
-        {
-            string content = (sender as Button).Content.ToString(); // Figures out button clicked.
-
-            for (int i = 0; i < listOfButtons.Count; i++)
+            if (getFilePath.Equals(false))
             {
-                if (listOfButtons.ElementAt(i).Content.ToString() == content)
-                {
-                    Process.Start(files[i]); // starts files at location where i was found.
-                }
+                MessageBox.Show("It Worked!");
             }
+            else
+            {
+                MessageBox.Show("YOURE STUPID");
+            }
+
+            MessageBox.Show(path);
+            
+
         }
 
 
